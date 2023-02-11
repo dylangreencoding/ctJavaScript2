bc0217d80b7e45d14ce277ddda152c4f8c0cccac
Dylan Green <dylangreencoding@gmail.com>
2023-02-11 16:52:37 -0500

    finished homework

--- script.js
+++ script.js
@@ -1,42 +1,106 @@
-`use strict`;
+// `use strict`;
 
-// #1 check for matched names - function assumes capitalization for names
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
 
-const dogString = `Hello Max, my name is Dog, and I have purple eyes!`;
-const dogNames = [`Max`, `HAS`, `PuRple`, `dog`];
+let person3 = {
+  pizza:["Deep Dish","South Side Thin Crust"],
+  tacos:"Anything not from Taco bell",
+  burgers:"Portillos Burgers",
+  ice_cream:["Chocolate","Vanilla","Oreo"],
+  shakes:[{
+      oberwise:"Chocolate",
+      dunkin:"Vanilla",
+      culvers:"All of them",
+      mcDonalds:"Sham-rock-shake",
+      cupids_candies:"Chocolate Malt"
+  }]
+}
 
-function findWords(dogString, dogNames) {
-  let matched = false;
-  for(let i = 0; i < dogNames.length; i++) {
-    dogNames[i] = dogNames[i].toLowerCase();
-    dogNames[i] = dogNames[i].charAt(0).toUpperCase() + dogNames[i].slice(1);
-    if(dogString.includes(dogNames[i])) {
-      matched = true;
-      console.log(`Matched ${dogNames[i]}`);
-    };
-  };
-  if(matched === false) {
-    console.log("No Matched Names");
-  };
-};
+// good opportunity to use a recursion function
 
-findWords(dogString, dogNames);
+const fxExpression = function(obj) {
+  for(let i = 0; i < Object.keys(obj).length; i++) {
+    if(Array.isArray(obj) === false) {
+      console.log(`Favorite ${Object.keys(obj)[i]}`);
+    }
+    let current = Object.values(obj)[i];
+    if(typeof current === 'string') {
+      console.log(`\t ${current}`);
+    } else if(typeof current === 'object') {
+      fxExpression(current);
+    }
+  }
+}
 
-// #2 replace even indexes - created new array & changed original in place, returned both
+// fxExpression(person3);
 
-const in_arr = [`Max`, `Baseball`, `Reboot`, `Goku`, `Trucks`, `Rodger`];
 
-function replaceEvens(arr) {
-  let newArr = [];
-  for(let i = 0; i < arr.length; i++) {
-    if(i % 2 === 0) {
-      newArr.push(`Even Index`);
-      arr[i] = 'EvenIndex';
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a
+printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called.
+Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons
+age by 3 years. Use an arrow function for both methods
+*/
+// Create our Person Prototype
+// Use an arrow to create the printInfo method
+// Create another arrow function for the addAge method that takes a single parameter
+// Adding to the age
+
+function Person(nom, age) {
+  this.nom = nom;
+  this.age = age;
+  this.birthday = () => this.age ++;
+  this.printInfo = () => console.log(`${this.nom} is ${this.age} years old!`);
+}
+
+const fred = new Person(`fred`, 23);
+// fred.printInfo();
+fred.birthday();
+// fred.printInfo();
+
+
+// =============Exercise #3 ============//
+/*
+
+  Create a Promised based function that will check a string to determine if it's length is greater than 10.
+  If the length is greater than ten console log "Big word". 
+  If the length of the string is less than 10 console log "Small Number"
+*/
+
+let output = document.getElementById('the-promised-land');
+let currentYarn = document.getElementById('yarn');
+let yarn;
+
+function measureYarn() {
+  return new Promise( (resolve, reject) => {
+    if (yarn.length > 10) {
+      resolve({ log: 'Big word' });
+    } else if (yarn.length < 10) {
+      resolve({ log: 'Small Number' });
     } else {
-      newArr.push(arr[i]);
-    };
-  };
-  return [newArr, arr];
-};
+      reject({ log: 'Exactly 10' })
+    }
+  })
+}
 
-console.log(replaceEvens(in_arr));
+currentYarn.addEventListener('keydown', (e) => { 
+  // console.log(e); // event
+  if (e.key === 'Enter') {
+    yarn = currentYarn.value;
+    // console.log(typeof yarn); // string
+    // console.log(yarn); // works - gets current value when entered
+    
+    measureYarn().then( (value) => {
+      output.innerHTML = value.log;
+    }).catch( (error) => {
+      output.innerHTML = error.log;
+    })
+  }
+})
